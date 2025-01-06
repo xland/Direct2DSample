da8bef432467bc2d01adc7df380d242d9f8c5a0b
unknown <412588801@qq.com>
2025-01-06 15:52:20 +0800

    transparent window

---
+++ 基于Layered样式的透明窗口/main.cpp
@@ -0,0 +1,72 @@
+#include <Windows.h>
+#include <windowsx.h>
+#include <blend2d.h>
+
+static int w{ 800 }, h{600};
+static HWND hwnd;
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
+    default:
+    {
+        break;
+    }
+    }
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+void paint()
+{
+    BLImage img(w, h, BL_FORMAT_PRGB32);
+    BLContext ctx(img);
+    ctx.fillAll(BLRgba32(0x22B83E52));
+    BLImageData data;
+    img.getData(&data);
+
+    HDC hdc = GetDC(hwnd);
+    auto compDC = CreateCompatibleDC(hdc);
+    auto bitmap = CreateCompatibleBitmap(hdc, w, h);
+    DeleteObject(SelectObject(compDC, bitmap));
+
+    BITMAPINFO info = { sizeof(BITMAPINFOHEADER), w, 0 - h, 1, 32, BI_RGB, w * 4 * h, 0, 0, 0, 0 };
+    SetDIBits(hdc, bitmap, 0, h, data.pixelData, &info, DIB_RGB_COLORS);
+    BLENDFUNCTION blend = { .BlendOp{AC_SRC_OVER}, .SourceConstantAlpha{255}, .AlphaFormat{AC_SRC_ALPHA} };
+    POINT pSrc = { 0, 0 };
+    SIZE sizeWnd = { w, h };
+    UpdateLayeredWindow(hwnd, hdc, NULL, &sizeWnd, compDC, &pSrc, NULL, &blend, ULW_ALPHA);
+    ReleaseDC(hwnd, hdc);
+
+    DeleteDC(compDC);
+    DeleteObject(bitmap);
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
+{
+    WNDCLASSEX wcx{};
+    wcx.cbSize = sizeof(wcx);
+    wcx.lpfnWndProc = WindowProc;
+    wcx.style = CS_HREDRAW | CS_VREDRAW;
+    wcx.hInstance = hInstance;
+    wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcx.lpszClassName = L"LayeredWindow";
+    RegisterClassEx(&wcx);
+    hwnd = CreateWindowEx(WS_EX_LAYERED, wcx.lpszClassName, wcx.lpszClassName, WS_CLIPCHILDREN | WS_CLIPSIBLINGS, 100, 100, w, h, NULL, NULL, hInstance, NULL);
+    paint();
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+    MSG msg = { 0 };
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    UnregisterClass(wcx.lpszClassName, hInstance);
+}
---
+++ 基于模糊API的透明窗口/main.cpp
@@ -0,0 +1,96 @@
+#include <d2d1.h>
+#include <dwmapi.h>
+#include <versionhelpers.h>
+
+static int w{ 800 }, h{600};
+static ID2D1Factory* d2d1Factory = nullptr;
+static ID2D1HwndRenderTarget* renderTarget = nullptr;
+
+bool EnableAlphaCompositing(HWND hWnd)
+{
+    if (!IsWindowsVistaOrGreater()) { return false; }
+    BOOL isCompositionEnable = false;
+    //检查DWM是否启用
+    DwmIsCompositionEnabled(&isCompositionEnable);
+    if (!isCompositionEnable) { return true; }
+    DWORD currentColor = 0;
+    BOOL isOpaque = false;
+    //检查是否支持毛玻璃效果
+    DwmGetColorizationColor(&currentColor, &isOpaque);
+    if (!isOpaque || IsWindows8OrGreater())
+    {
+        HRGN region = CreateRectRgn(0, 0, -1, -1);
+        DWM_BLURBEHIND bb = { 0 };
+        bb.dwFlags = DWM_BB_ENABLE | DWM_BB_BLURREGION;
+        bb.hRgnBlur = region;
+        bb.fEnable = TRUE;
+        DwmEnableBlurBehindWindow(hWnd, &bb);
+        DeleteObject(region);
+        return true;
+    }
+    else // For Window7
+    {
+        DWM_BLURBEHIND bb = { 0 };
+        bb.dwFlags = DWM_BB_ENABLE;
+        DwmEnableBlurBehindWindow(hWnd, &bb);
+        return false;
+    }
+}
+
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    if (uMsg == WM_PAINT)
+    {
+        renderTarget->BeginDraw();
+        renderTarget->Clear(D2D1::ColorF(0.2f, 0.3f, 0.5f, 0.5f));
+        renderTarget->EndDraw();
+        return 0;
+    }
+    else if (uMsg == WM_SIZING) {
+        RECT* pRect = reinterpret_cast<RECT*>(lParam);
+        w = pRect->right - pRect->left;
+        h = pRect->bottom - pRect->top;
+        auto size = D2D1::SizeU(w, h);
+        renderTarget->Resize(size);
+        return 0;
+    }
+    else if (uMsg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
+{
+    WNDCLASSEXW wcx = {};
+    wcx.cbSize = sizeof(wcx);
+    wcx.lpfnWndProc = WindowProc;
+    wcx.lpszClassName = L"Direct2DSample";
+    wcx.hInstance = hInstance;
+    wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
+    RegisterClassEx(&wcx);
+    HWND hwnd = CreateWindowEx(NULL, wcx.lpszClassName, L"SampleWindow", WS_OVERLAPPEDWINDOW,
+        200, 200, w, h,nullptr, nullptr, hInstance, nullptr);
+    EnableAlphaCompositing(hwnd);
+    {
+        D2D1_FACTORY_OPTIONS options = {};
+        options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
+        D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED,options,&d2d1Factory);
+        auto pixelFormat = D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED);
+        auto renderProps = D2D1::RenderTargetProperties(D2D1_RENDER_TARGET_TYPE_DEFAULT,pixelFormat);
+        auto size = D2D1::SizeU(w, h);
+        auto hwndRenderProps = D2D1::HwndRenderTargetProperties(hwnd, size);
+        d2d1Factory->CreateHwndRenderTarget(renderProps, hwndRenderProps, &renderTarget);        
+    }
+    ShowWindow(hwnd, SW_NORMAL);
+    MSG msg = { 0 };
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    renderTarget->Release();
+    d2d1Factory->Release();
+    UnregisterClass(wcx.lpszClassName, hInstance);
+}
