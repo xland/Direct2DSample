c6292a9a8475a3f2745997a7f6450ad03458883e
unknown <412588801@qq.com>
2025-01-07 17:32:51 +0800

    Update main.cpp

--- 无重定向表面的透明窗口/main.cpp
+++ 无重定向表面的透明窗口/main.cpp
@@ -17,32 +17,30 @@ ComPtr<ID2D1RenderTarget> renderTarget;
 ComPtr<IDCompositionDevice> compositionDevice;
 ComPtr<IDCompositionTarget> compositionTarget;
 ComPtr<IDCompositionVisual> compositionVisual;
-ComPtr<ID2D1SolidColorBrush> brush;
 
 
 void initD2D() {
     D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, D3D11_CREATE_DEVICE_SINGLETHREADED | D3D11_CREATE_DEVICE_BGRA_SUPPORT, NULL, 0, D3D11_SDK_VERSION, &d3dDevice, NULL, NULL);
     d3dDevice.As(&dxgiDevice);
     ComPtr<IDXGIFactory2> dxgiFactory;
-    CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, __uuidof(dxgiFactory), reinterpret_cast<void**>(dxgiFactory.GetAddressOf()));
+    CreateDXGIFactory2(0, __uuidof(dxgiFactory), reinterpret_cast<void**>(dxgiFactory.GetAddressOf()));
 
     DXGI_SWAP_CHAIN_DESC1 description = {};
+    description.Width = w;
+    description.Height = h;
     description.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
     description.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
     description.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
     description.BufferCount = 2;
     description.SampleDesc.Count = 1;
     description.AlphaMode = DXGI_ALPHA_MODE_PREMULTIPLIED;
-    description.Width = w;
-    description.Height = h;
     dxgiFactory->CreateSwapChainForComposition(dxgiDevice.Get(), &description, nullptr, swapChain.GetAddressOf());
     dxgiFactory->MakeWindowAssociation(hwnd, DXGI_MWA_NO_ALT_ENTER);
 
     ComPtr<IDXGISurface2> dxgiSurface;
     swapChain->GetBuffer(0, __uuidof(dxgiSurface), reinterpret_cast<void**>(dxgiSurface.GetAddressOf()));
 
-    D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, d2dFactory.GetAddressOf());
-    
+    D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, d2dFactory.GetAddressOf());    
     D2D1_RENDER_TARGET_PROPERTIES targetProperties = {};
     targetProperties.type = D2D1_RENDER_TARGET_TYPE_DEFAULT;
     targetProperties.pixelFormat.format = DXGI_FORMAT_UNKNOWN;
@@ -55,10 +53,6 @@ void initD2D() {
     compositionVisual->SetContent(swapChain.Get());
     compositionTarget->SetRoot(compositionVisual.Get());
     compositionDevice->Commit();
-
-
-    D2D1_COLOR_F const brushColor = D2D1::ColorF(0.18f, 0.55f, 0.34f, 0.75f); // rgba
-    renderTarget->CreateSolidColorBrush(brushColor, brush.GetAddressOf());
 }
 
 
@@ -69,13 +63,9 @@ LRESULT CALLBACK winProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hWnd, &ps);
-
         renderTarget->BeginDraw();
-        renderTarget->Clear();
-        D2D1_POINT_2F const ellipseCenter = D2D1::Point2F(w-160, h-200); // y
-        D2D1_ELLIPSE const ellipse = D2D1::Ellipse(ellipseCenter, 100.0f, 100.0f);
-        renderTarget->FillEllipse(ellipse, brush.Get());
-        renderTarget->EndDraw(); // Make the swap chain available to the composition engine
+        renderTarget->Clear(D2D1::ColorF(0.6f, 0.6f, 0.2f, 0.5f));
+        renderTarget->EndDraw();
         swapChain->Present(1, 0);
         EndPaint(hWnd, &ps);
         return 0;
